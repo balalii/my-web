7a2d27086da0cdf55ed32632645a3fcfc9cf9de3
Iqbal Ali <iqbalali2606@gmail.com>
2023-11-07 22:59:59 +0700

    style : Header Responsive

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -4,17 +4,17 @@ export default function Header() {
   return (
     <div className="  text-[var(--primary-color)] ">
       <h1
-        className="text-rigth font-[600] flex flex-col items-start max-w-[18rem] 
+        className="text-rigth font-[600] flex flex-col items-start max-w-[22rem] 
       md:max-w-[43rem] mx-auto lg:max-w-none lg:mx-0
-      text-[60px] leading-[60px]
+      text-[65px] leading-[65px]
       md:text-[140px] md:leading-[134px] 
       lg:text-[190px] lg:leading-[164px] 
       
       "
       >
-        <span className="tracking-[2px] md:tracking-[4px] lg:tracking-[2px]">PORTFOLIO </span>
+        <span className="tracking-[3px] md:tracking-[4px] lg:tracking-[2px]">PORTFOLIO </span>
         <span className="self-end lg:tracking-[0px] ">FRONT</span>
-        <span className="tracking-[px] lg:tracking-[-3px]">{'END DEV</>'}</span>
+        <span className="tracking-[1px] lg:tracking-[-3px]">{'END DEV</>'}</span>
       </h1>
       <ThemeBtn />
     </div>
--- src/components/ThemeBtn/ThemeBtn.tsx
+++ src/components/ThemeBtn/ThemeBtn.tsx
@@ -15,7 +15,7 @@ export default function ThemeBtn() {
   return (
     <label
       className="inline-flex relative z-10  items-center space-x-4 cursor-pointer text-[var(--primary-color)]
-     -top-[112px] left-[2rem]
+     -top-[119px] left-[2rem]
       md:-top-[253px] md:left-[3rem]
       lg:-top-[313px] lg:left-[2rem]
       
@@ -25,14 +25,14 @@ export default function ThemeBtn() {
         <input id="Toggle1" type="checkbox" className="hidden peer" checked={theTheme !== 'light'} onChange={toggleTheme} />
         <div
           className="rounded-full shadow-inner bg-[var(--primary-color)]
-          w-[80px] h-[39px]
+          w-[80px] h-[42px]
           md:w-[200px] md:h-[100px]
           lg:w-[280px] lg:h-[130px]
           "
         ></div>
         <div
           className={`absolute inset-y-0 left-0  rounded-full shadow bg-[var(--background-color)] peer-checked:right-0 peer-checked:left-auto
-          w-[29px] h-[29px] m-[5px]
+          w-[32px] h-[32px] m-[5px]
           md:w-[70px] md:h-[70px] md:m-[15px] 
           lg:w-[90px] lg:h-[90px] lg:m-[20px] 
           `}
